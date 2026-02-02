54d22455e3b1b549711e978d2b59b55584e10816
leandroltfx <leandroltfbr@hotmail.com>
2026-02-02 08:15:42 -0300

    Inicia o projeto

---
+++ src/main/java/br/com/links_vault_core_back_springboot/LinksVaultCoreBackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.links_vault_core_back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksVaultCoreBackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksVaultCoreBackSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/links_vault_core_back_springboot/LinksVaultCoreBackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.links_vault_core_back_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksVaultCoreBackSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
